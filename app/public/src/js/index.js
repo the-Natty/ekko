fa381fa659562b5e913bf9ab8c627fa467beb0d8
chuck <cxf1028949929@163.com>
2018-02-16 21:17:55 +0800

    index page

--- app/public/src/js/index.js
+++ app/public/src/js/index.js
@@ -1,1 +1,11 @@
-console.log(233); 
+var setbodyHeight = function(){
+    $('.container').css('height', document.documentElement.clientHeight + 'px');
+}
+
+window.onload = setbodyHeight;
+window.onresize = setbodyHeight;
+
+$('body')
+    .on('click', '.J_unOpen', function(){
+        swal('技能,作品,联系我 暂未开放入口！');
+    })
