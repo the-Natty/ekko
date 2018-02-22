0d80eac9a5912cb79c1212bdde37be16023e941a
陈小飞 <cxf15062@ly.com>
2018-02-22 14:41:40 +0800

    tj

--- app/public/src/js/index.js
+++ app/public/src/js/index.js
@@ -6,6 +6,12 @@ var setbodyHeight = function(){
 window.onload = setbodyHeight;
 window.onresize = setbodyHeight;
 
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    });
+});
+
 $('body')
     .on('click', '.J_unOpen', function(){
         swal('技能,作品,联系我 暂未开放入口！');
