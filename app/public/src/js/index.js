348d7a1a04f00fccc09db74c05444d89e8d6c167
陈小飞 <cxf15062@ly.com>
2018-02-22 15:32:42 +0800

    tj

--- app/public/src/js/index.js
+++ app/public/src/js/index.js
@@ -1,11 +1,3 @@
-var setbodyHeight = function(){
-    $('.container').css('height', document.documentElement.clientHeight + 49 + 'px');
-    $('.J_wel').removeClass('none');
-}
-
-window.onload = setbodyHeight;
-window.onresize = setbodyHeight;
-
 $(document).ready(function() {
 	$('#fullpage').fullpage({
         sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke'],
